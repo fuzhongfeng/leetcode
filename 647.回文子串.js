2fa9b2079bde87d2cc4ee68461cdd12f2473905d
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-10-28 09:05:09 +0800

    回文子串 solution

--- 647.回文子串.js
+++ 647.回文子串.js
@@ -10,7 +10,23 @@
  * @return {number}
  */
 var countSubstrings = function(s) {
-    
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        // 处理个数为奇数情况的回文，如："aba"
+        result += iterate(s, i, i);
+        // 处理个数为偶数情况的回文，如："aa"
+        result += iterate(s, i, i + 1);
+    }
+    return result;
 };
+
+const iterate = (s, start, end) => {
+    let num = 0;
+    while(start >= 0 && end < s.length && s[start--] === s[end++]) {
+        num++;
+    }
+    return num;
+}
+
 // @lc code=end
 
