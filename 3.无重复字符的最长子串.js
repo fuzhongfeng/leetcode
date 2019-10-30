08f66683bd7eb6841a3fd0f8888e18ed0e0c1e57
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-10-30 14:19:54 +0800

    无重复字符的最长子串算法优化

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -10,14 +10,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
+    // 这里的子串要求是连续的
     let len = 0;
-    for (let i=0; i<s.length; i++) {
-        let sub = "";
-        let j = i;
-        while(j < s.length && sub.indexOf(s[j]) === -1) {
-            sub += s[j++];
+    let sub = ""; // 利用变量存储
+    for (let i = 0; i < s.length; i++) {
+        let index = sub.indexOf(s[i])
+        sub += s[i];
+
+        if (index !== -1) { // 如果有重复的则重置 sub 变量
+            sub = sub.substr(index + 1); // 注意这里截取的开始位置为已重复的下一位。也就是将重复位置及之前的全部丢弃。
         }
-        len = Math.max(len, sub.length);
+
+        len = Math.max(len, sub.length)
     }
     return len;
 };
