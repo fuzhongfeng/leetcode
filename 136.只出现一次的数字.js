6e6796c5a3d8a3d0599c5d85679b22b5c3f93823
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-11-04 21:26:42 +0800

    136添加注释

--- 136.只出现一次的数字.js
+++ 136.只出现一次的数字.js
@@ -10,9 +10,15 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+    // 任意数 n m
+    // 按位异或 ^ : 对每一对比特位执行异或（XOR）操作。当位 n 和位 m 不相同时，n XOR m 的结果为 1
+    // n ^ 0 = n
+    // n ^ n = 0
+    // n^m^n = (n^n)^m = 0^b = b
+
     let r = 0;
     for (let n of nums) {
-        r = n ^ r
+        r ^= n;
     }
     return r;
 };
