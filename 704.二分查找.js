e7d7c44fe090ff9be87f4cbe8e9d68e6761f2458
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-11-09 22:01:07 +0800

    二分查找补充

--- 704.二分查找.js
+++ 704.二分查找.js
@@ -6,23 +6,27 @@
 
 // @lc code=start
 /**
+ * 二分查找的局限性：
+ * 1. 单调递增或递减
+ * 2. 存在上下界
+ * 3. 通过索引可以访问
+ * 有序数组可以适用，链表不适用
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
+	let left = 0;
     let right = nums.length - 1;
-    
-    while(left <= right) {
-        // left + 是防止 left > right 时出现死循环
-        let mid = left + Math.floor((right - left) / 2);
+
+    while (left <= right) {
+		const mid = left + Math.floor((right - left)/2)
         if (nums[mid] === target) {
             return mid;
         } else if (nums[mid] < target) {
             left = mid + 1;
         } else {
-            right = mid -1;
+            right = mid - 1;
         }
     }
     return -1;
