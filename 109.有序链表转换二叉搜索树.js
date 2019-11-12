24a5f749ccf16c06fbcd27e22f8274d67cf73ee2
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-11-12 21:43:19 +0800

    打家劫舍solution

--- 109.有序链表转换二叉搜索树.js
+++ 109.有序链表转换二叉搜索树.js
@@ -24,7 +24,6 @@
  * @return {TreeNode}
  */
 var sortedListToBST = function(head) {
-    
 };
 // @lc code=end
 
---
+++ 198.打家劫舍.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    // f(n) = Max(f(n-2) + Vn, f(n-1))
+    let per = 0;
+    let max = 0;
+    for(let i=0; i<nums.length; i++) {
+      const temp = max;
+      max = Math.max(per + nums[i], max)
+      per = temp;
+    }
+    return max;
+};
+// @lc code=end
+
