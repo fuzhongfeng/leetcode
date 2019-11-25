616c27bbea1e8dd9369d1cdabe479441e6ec7032
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-11-25 10:33:12 +0800

    最大子序和

--- 53.最大子序和.js
+++ 53.最大子序和.js
@@ -10,7 +10,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    let r = nums[0];
+    let sum = 0;
+    for(let i=0; i<nums.length; i++) {
+      sum = Math.max(nums[i], sum + nums[i])
+      r = Math.max(sum, r)
+    }
+    return r;
 };
 // @lc code=end
 
