e9414a7ebd6bd6409dbc7a288c4e929d3a7250bc
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-11-28 10:59:18 +0800

    firstUniqChar solution

--- 387.字符串中的第一个唯一字符.js
+++ 387.字符串中的第一个唯一字符.js
@@ -10,14 +10,56 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let r = -1
+    // let r = -1
+    // for (let i = 0; i < s.length; i++) {
+    //   if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+    //     r = i;
+    //     break;
+    //   }
+    // }
+    // return r;
+
+    let r = -1;
+    let map = new Map();
     for (let i = 0; i < s.length; i++) {
-      if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+      if (map.has(s[i])) {
+        map.set(s[i], true);
+      } else {
+        map.set(s[i], 1);
+      }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+      if (map.get(s[i]) === 1) {
         r = i;
         break;
       }
     }
     return r;
+
+    // let result = -1;
+    // const map = new Map();
+    // let len = s.length;
+
+    // let len1 = 0;
+    // while(len1 < len) {
+    //   if (map.has(s[len1])) {
+    //     map.set(s[len1], false);
+    //   } else {
+    //     map.set(s[len1], true);
+    //   }
+    //   len1++;
+    // }
+
+    // let len2 = 0;
+    // while(len2 < len) {
+    //   if (map.get(s[len2])) {
+    //     result = len2;
+    //     break;
+    //   }
+    //   len2++;
+    // }
+    // return result;
 };
 // @lc code=end
 
