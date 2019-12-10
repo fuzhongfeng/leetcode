c62a09ec9eb66dd9177d25cb5cb7f9f54587a3ea
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-12-10 23:04:57 +0800

    爬楼梯

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -10,11 +10,13 @@
  * @return {number}
  */
 var climbStairs = function(n) {
+    // 动态转移方程
+    // dp[n] = dp[n-1] + dp[n-2]
     if (n < 1) return 0;
     if (n === 1) return 1; 
     if (n === 2) return 2;
-    let min = 1;
-    let max = 2;
+    let min = 1; // dp[n-2]
+    let max = 2; // dp[n-1]
     let total = 0;
     for (let i = 3; i <= n; i++) {
         total = min + max;
