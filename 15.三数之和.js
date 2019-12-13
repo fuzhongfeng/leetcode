a3f1984ae0136a16bd3e94474931986fa3b6267f
fuzhongfeng <fuzhongfeng@gaosiedu.com>
2019-12-13 10:59:24 +0800

    three sum

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -10,7 +10,23 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+    // 1. 哈希表
+    const map = new Map();
+    const result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+      for (let j = i + 1; j < nums.length - 1; j++) {
+        if (map.has(nums[j])) {
+          result.push(map.get(nums[j]).concat([nums[j]]))
+          map.delete(nums[j])
+        } else {
+          const n = 0 - nums[j] - nums[i]
+          map.set(n, [nums[j], nums[i]])
+        }
+      }
+    }
+
+    return result;
 };
 // @lc code=end
 
---
+++ 923.三数之和的多种可能.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=923 lang=javascript
+ *
+ * [923] 三数之和的多种可能
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} A
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumMulti = function(A, target) {
+    
+};
+// @lc code=end
+
